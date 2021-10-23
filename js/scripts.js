3489cd78f00fe397ba1ebc4c44f22697b8232902
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-23 23:28:09 +0300

    Addition of click functionality to the What We do section. Fixed the form default submit function in order to display the nameInput as an alert for now

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  $(".designImgText").click(function() {
+    $(".designImgText").toggle(1000, "linear");
+    $(".designDescription").toggle(1000, "linear");
+  });
+  $(".developmentImgText").click(function() {
+    $(".developmentImgText").toggle(1000, "linear");
+    $("#developmentDescription").toggle(1000, "linear");
+  });
+
     $("#userInput form").submit(function(event) {
       var nameInput = $("#nameInput").val();
       alert(nameInput);
