ec1d0b3547d889cc1a9fcc295d6a9aa44e7b752c
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-24 21:16:20 +0300

    Attempt at shortening the code needed to display the text over the images.

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,17 @@ $(document).ready(function() {
     $(".productManagementImgText").toggle(1000, "linear");
     $(".productManagementDescription").toggle(1000, "linear");
   });
+  var portfolioImages = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8"]
+  var hoverText = ["hover1", "hover2", "hover3", "hover4", "hover5", "hover6", "hover7", "hover8"]
+  porfolioImages.forEach(function(image) {
+    $("#" + image).hover(function() {
+      hoverText.forEach(function(text) {
+      $("#" + text).show().offset($("#" + image).offset());
+    }, function() {
+      $("#" + text).hide();
+    });
+  };
+});
   $("#img1").hover(function() {
     $("#hover1").show().offset($("#img1").offset());
   }, function() {
