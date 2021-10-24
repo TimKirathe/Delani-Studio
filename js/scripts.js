2275f08f3129517dd56aa72feea9799447883051
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-24 20:45:17 +0300

    Added the receival message of information for the user. Included the text for the What We Do section images on click. Added the white box display of the project name on hover.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,34 @@ $(document).ready(function() {
   });
   $(".developmentImgText").click(function() {
     $(".developmentImgText").toggle(1000, "linear");
-    $("#developmentDescription").toggle(1000, "linear");
+    $(".developmentDescription").toggle(1000, "linear");
+  });
+  $(".productManagementImgText").click(function() {
+    $(".productManagementImgText").toggle(1000, "linear");
+    $(".productManagementDescription").toggle(1000, "linear");
+  });
+  $(".designDescription").click(function() {
+    $(".designImgText").toggle(1000, "linear");
+    $(".designDescription").toggle(1000, "linear");
+  });
+  $(".developmentDescription").click(function() {
+    $(".developmentImgText").toggle(1000, "linear");
+    $(".developmentDescription").toggle(1000, "linear");
+  });
+  $(".productManagementDescription").click(function() {
+    $(".productManagementImgText").toggle(1000, "linear");
+    $(".productManagementDescription").toggle(1000, "linear");
+  });
+  $("#img1").hover(function() {
+    $("#hover1").show().offset($("#img1").offset());
+  }, function() {
+    $("#hover1").hide();
   });
 
     $("#userInput form").submit(function(event) {
       var nameInput = $("#nameInput").val();
       alert(nameInput);
+      $("#receivedMessage").text("Thank you " + nameInput + " for your message. We have received it.");
       event.preventDefault();
     });
   });
